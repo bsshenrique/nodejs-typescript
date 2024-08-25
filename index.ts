244f5624461eed5e41b233419d310f87f82a9680
Henrique <45628939+bsshenrique@users.noreply.github.com>
2024-08-25 19:56:52 -0300

    chore: implementa o exemplo do repositorio (#1)
    * build: define o pacote do repositorio
    
    * chore: define a licenca do repositorio
    
    * chore: cria a configuracao para o tsconfig
    
    * feat: implementa o exemplo
    
    * chore: altera o host default para facilitar o expose da aplicacao
    
    * build: implementa o Dockerfile

---
+++ index.ts
@@ -0,0 +1,27 @@
+import http from 'node:http';
+
+process.loadEnvFile('./development.env');
+
+const SERVER_HOST = process.env.SERVER_HOST || '0.0.0.0';
+const SERVER_PORT = Number(process.env.SERVER_PORT || 3000);
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+  const method = request.method;
+  const path = request.url;
+  let statusCode = 404;
+
+  if (method === 'GET' && path === '/') {
+    statusCode = 200;
+  }
+
+  response.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+  response.end(JSON.stringify(http.STATUS_CODES[statusCode]));
+});
+
+server.on('listening', () => {
+  console.info(`Server listening on ${SERVER_HOST}:${SERVER_PORT}`);
+});
+
+server.listen(SERVER_PORT, SERVER_HOST);
